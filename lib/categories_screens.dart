772d502e84412349ebbd018423f8920c0f2e371c
Byron Kimani <byronkimani@gmail.com>
2020-06-29 17:57:45 +0300

    add theme and fonts

---
+++ lib/categories_screens.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals/category_item.dart';
+import 'package:meals/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+      ),
+    );
+  }
+}
